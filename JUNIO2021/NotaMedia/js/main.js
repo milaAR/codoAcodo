b6e42d48836d0c447130d7410a53b0fe141eb80c
milaAR <78050834+milaAR@users.noreply.github.com>
2021-06-01 14:06:32 +0200

    Add files via upload

---
+++ JUNIO2021/NotaMedia/js/main.js
@@ -0,0 +1,7 @@
+let nombre=prompt("Ingrese el nombre del alumno");
+let nota1=Number(prompt("Ingrese la primer nota"));
+let nota2=Number(prompt("Ingrese la segunda nota"));
+let nota3=Number(prompt("Ingrese la tercera nota"));
+let promedio=(nota1+nota2+nota3)/3;
+
+alert(`El alumno ${nombre} tiene de promedio ${promedio}`)
---
+++ JUNIO2021/mediaJS/js/main.js
@@ -0,0 +1,4 @@
+let valor1=40;
+let valor2=16;
+let sumaTotal=valor1+valor2;
+document.getElementById("resultadoCalculo").innerHTML=`El resultado de la suma de las dos variables es: ${sumaTotal}`;
